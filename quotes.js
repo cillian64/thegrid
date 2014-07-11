91da6ca523bf22be852500affb0c78a40676e322
David Turner <david.w.turner@cl.cam.ac.uk>
2014-07-11 15:59:57 +0000

    Update documentation

---
+++ quotes.js
@@ -0,0 +1,7 @@
+var quote = new Array()
+quote[0] = "A mad forest of noisy LEDs"
+quote[1] = "We'd like to buy a large quantity of LED strip..."
+quote[2] = "What's the cheapest way to get 900W at 12V?"
+quote[3] = "I didn't expect the truck to be so big!"
+var qnum = quote.length
+pickquote = Math.round(Math.random()*(qnum-1))
