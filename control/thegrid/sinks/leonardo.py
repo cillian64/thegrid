f3af22db0ba7be122e4cdb777076c18af48dc540
David Turner <cillian64@googlemail.com>
2014-07-30 00:12:59 +0100

    Leonardo firmware and sink

---
+++ control/thegrid/sinks/leonardo.py
@@ -0,0 +1,42 @@
+"""
+leonardo.py
+
+Hardware sink to sen dcurrent grid state to the shift registers using an
+Arduino Leonardo running the leonardo.ino sketch.
+"""
+
+import logging
+logger = logging.getLogger(__name__)
+
+try:
+    import serial
+except ImportError:
+    logger.warning("Could not import Serial, aborting leonardo sink")
+    serial = None
+
+from .sink import Sink, register_sink
+
+
+
+@register_sink("Leonardo")
+class Leonardo(Sink):
+    def __init__(self):
+        if serial is not None:
+            self.ser = serial.Serial("/dev/ttyACM0", 115200)
+            logger.info("Leonardo sink initialised")
+        else:
+            self.ser = None
+            logger.error("Can't initialise leonardo sink, no serial library")
+
+    def update(self, state):
+        if self.ser is None:
+            return
+
+        output = bytearray([2, 0, 0, 0, 0, 0, 0, 0, 3])
+        for colidx, column in enumerate(state.T):
+            tot = 0
+            for rowidx, bit in enumerate(column):
+                tot += bit * 2**rowidx
+            output[colidx+1] = tot
+        self.ser.write(output)
+
---
+++ leonardo/leonardo.ino
@@ -0,0 +1,70 @@
+// In arduino-1.0.x/hardware/arduino/cores/arduino/CDC.cpp
+// change SERIAL_BUFFER_SIZE to 2048 to prevent buffer overflow
+// on large CDC incoming packets (since 1pkt/ms we have time to
+// write them to the bus eventually but need more buffer to 
+// hold them beforehand. note that 2.5k ram on this chip so
+// 2k is a lot of it, but this code only uses 155 bytes so we're
+// OK really)
+
+const int datapin = 2;
+const int srckpin = 1;
+const int rckpin = 0;
+
+const int ledpin = 13;
+
+void setup()
+{
+  Serial.begin(115200);
+  
+  pinMode(datapin, OUTPUT);
+  pinMode(srckpin, OUTPUT);
+  pinMode(rckpin, OUTPUT);
+  pinMode(ledpin, OUTPUT);
+
+  digitalWrite(srckpin, LOW);
+  digitalWrite(rckpin, LOW);
+  digitalWrite(ledpin, LOW);
+}
+
+void wait()
+{
+  delayMicroseconds(1);
+}
+
+// Clocks out the LSB of thebit using srck
+void txbit(int thebit)
+{
+    digitalWrite(datapin, thebit & 1);
+    wait();
+    digitalWrite(srckpin, HIGH);
+    wait();
+    digitalWrite(srckpin, LOW);
+    wait();
+}
+
+// Clocks out thebyte, LSB first.
+void txbyte(int thebyte)
+{
+    txbit(0); // First bit is ignored
+    for(int i=0; i<7; i++)
+      txbit(thebyte >> i);
+}
+
+void loop()
+{
+  int rows[7];
+
+  while(!Serial.available() || Serial.read() != 0x02); // Wait for start byte
+  while(Serial.available() < 8); // Wait for a packet
+  for(int i=0; i<7; i++)
+      rows[i] = Serial.read();
+  if(Serial.read() == 0x03)
+  {
+    for(int i=0; i<7; i++)
+      txbyte(rows[i]);
+    digitalWrite(rckpin, HIGH);
+    wait();
+    digitalWrite(rckpin, LOW);
+    wait();
+  }
+}
