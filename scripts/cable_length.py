e1844e49f7b8b7c9951842f124fd52055342d6dd
Adam Greig <adam@adamgreig.com>
2014-08-10 00:45:29 +0100

    update script to bin cables into reels

--- scripts/cable_length.py
+++ scripts/cable_length.py
@@ -1,12 +1,29 @@
-acc = 0
+lengths = []
 for x in range(7):
     for y in range(7):
         xlength = 12 - 2 * x
         ylength = 2 * abs(y - 3)
         pad = 2.5
         total = xlength + ylength + pad
-        acc += total
+        lengths.append(total)
         print("LED ({}, {}): {}m + {}m + {}m = {}m".format(
               x, y, xlength, ylength, pad, total))
 
-print("Total length: {}".format(acc))
+print("\nTotal length: {}\n".format(sum(lengths)))
+
+bins = [[], [], [], [], [], []]
+unbinned = []
+
+for length in reversed(sorted(lengths)):
+    for b in bins:
+        if 100.0 - sum(b) >= length:
+            b.append(length)
+            break
+    else:
+        unbinned.append(length)
+
+for idx, b in enumerate(bins):
+    print("Bin {}: {}".format(idx, ', '.join(["{}m".format(l) for l in b])))
+    print("Total length: {}m\n".format(sum(b)))
+
+print("Unbinned: {}".format(', '.join(["{}m".format(l) for l in unbinned])))
