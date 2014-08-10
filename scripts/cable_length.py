2e3ef12128246a24c62638baef9fb1fe1a5d2771
David Turner <cillian64@googlemail.com>
2014-08-10 14:19:39 +0100

    Cutting list

--- scripts/cable_length.py
+++ scripts/cable_length.py
@@ -1,29 +1,42 @@
-lengths = []
+cuts = []
+
 for x in range(7):
     for y in range(7):
         xlength = 12 - 2 * x
         ylength = 2 * abs(y - 3)
         pad = 2.7
         total = xlength + ylength + pad
-        lengths.append(total)
+        cuts.append((total, x, y))
         print("LED ({}, {}): {}m + {}m + {}m = {}m".format(
               x, y, xlength, ylength, pad, total))
 
-print("\nTotal length: {:.1f}\n".format(sum(lengths)))
+print("\nTotal length: {:.1f}\n".format(sum([c[0] for c in cuts])))
 
 bins = [[], [], [], [], [], []]
 unbinned = []
 
-for length in reversed(sorted(lengths)):
+for cut in reversed(sorted(cuts, key=lambda cut: cut[0])):
+    length = cut[0]
     for b in bins:
-        if 100.0 - sum(b) >= length:
-            b.append(length)
+        if 100.0 - sum([c[0] for c in b]) >= length:
+            b.append(cut)
             break
     else:
-        unbinned.append(length)
+        unbinned.append(cut)
 
 for idx, b in enumerate(bins):
-    print("Bin {}: {}".format(idx, ', '.join(["{}m".format(l) for l in b])))
-    print("Total length: {:.1f}m\n".format(sum(b)))
+    print("Bin {}: {}".format(idx, ', '.join(
+        ["{:.2f}m".format(c[0]) for c in b])))
+    print("Total length: {:.1f}m\n".format(sum([c[0] for c in b])))
 
-print("Unbinned: {}".format(', '.join(["{:.1f}m".format(l) for l in unbinned])))
+print("Unbinned: {}".format(', '.join([" ".format(c[0]) for c in unbinned])))
+
+# Cutting list:
+print("")
+print("")
+print("--== CUTTING LIST ==--")
+for idx, b in enumerate(bins):
+    print("--- Reel {} ---".format(idx+1))
+    for cut in b:
+        print("({},{}): {:.2f}m\t[ ]".format(cut[1], cut[2], cut[0]))
+    print("")
