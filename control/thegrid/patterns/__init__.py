d137988c39b9e5d66c20997f20d28ea1638a40d6
David Turner <cillian64@googlemail.com>
2014-08-31 15:19:46 +0100

    Further opencv gracefulness

--- control/thegrid/patterns/__init__.py
+++ control/thegrid/patterns/__init__.py
@@ -8,7 +8,12 @@ Add any new patterns here with `from . import <name>`.
 from . import pong, amaze, sparkle, playlist, static_patterns, on, sample
 from . import pattern_one, strike, lightning
 from .musicpatterns import thegrid
-from . import simplecv
+try:
+    import cv2
+except:
+    pass
+else:
+    from . import simplecv
 
 from .pattern import loaded_patterns
 
