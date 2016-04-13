0d9f173e81cd608abf4c6e384ebd1cf591db84e5
Adam Greig <adam@adamgreig.com>
2016-04-13 23:39:24 +0100

    add DAC to drive buzzer

--- polefw/mcuconf.h
+++ polefw/mcuconf.h
@@ -98,10 +98,12 @@
 #define STM32_GPT_USE_TIM1                  FALSE
 #define STM32_GPT_USE_TIM2                  FALSE
 #define STM32_GPT_USE_TIM3                  FALSE
+#define STM32_GPT_USE_TIM6                  TRUE
 #define STM32_GPT_USE_TIM14                 FALSE
 #define STM32_GPT_TIM1_IRQ_PRIORITY         2
 #define STM32_GPT_TIM2_IRQ_PRIORITY         2
 #define STM32_GPT_TIM3_IRQ_PRIORITY         2
+#define STM32_GPT_TIM6_IRQ_PRIORITY         2
 #define STM32_GPT_TIM14_IRQ_PRIORITY        2
 
 /*
