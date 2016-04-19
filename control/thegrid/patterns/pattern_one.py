209797effecc631fab5d83d255cd2b91e00e8fa1
David Turner <david.w.turner@cl.cam.ac.uk>
2016-04-19 13:53:57 +0100

    Converted pattern_one.py to generators as demo

--- control/thegrid/patterns/pattern_one.py
+++ control/thegrid/patterns/pattern_one.py
@@ -7,52 +7,51 @@ grid_size = 7
 
 @register_pattern("PatternScan")
 class PatternScan(Pattern):
-    count = 0
+    def __init__(self, cfg, tracking):
+        self.gen = self.generator()
+
+    def generator(self):
+        lights = np.zeros((7,7), dtype=np.bool)
+        while True:
+            for count in range(128):
+                lights[:][:] = 0
+                for i in range(7):
+                    if (count & (1 << i)):
+                        lights[i][i] = 1
+                yield lights, 0.1
 
     def update(self):
-        lights = np.zeros((7,7), dtype=np.bool)
-
-        for i in range(7):
-            if (self.count & (1 << i)):
-                lights[i][i] = 1
-
-        self.count+=1
-
-        if (self.count >= 128):
-            self.count = 0
-
-        # Return, get called again in 100ms
-        delay = 0.1
-        return lights, delay
+        return self.gen.__next__()
 
 @register_pattern("PatternSpin")
 class PatternSpin(Pattern):
-    count = 0
-    phase = 0
+    def __init__(self, cfg, tracking):
+        self.gen = self.generator()
 
     def update(self):
+        return self.gen.__next__()
+
+    def generator(self):
         lights = np.zeros((7,7), dtype=np.bool)
-
-        if self.phase == 0: # Up the y axis
-            lights[0][self.count] = 1
-        elif self.phase == 1: # Across the top
-            lights[self.count][grid_size-1] = 1
-        elif self.phase == 2: # Down the side
-            lights[grid_size-1][(grid_size-1)-self.count] = 1
-        elif self.phase == 3: # Back accross the bottom
-            lights[(grid_size-1)-self.count][0] = 1
-
-        self.count+=1
-
-        # Move the self.count
-        if self.count >= grid_size:
-            self.count = 0
-            self.phase+=1
-
-            # Move through the self.phases
-            if self.phase >= 4:
-                self.phase = 0
-
-        # Return, get called again in 100ms
         delay = 0.1
-        return lights, delay
+        while True:
+            for count in range(7): # phase 0, up y axis
+                lights[:][:] = 0
+                lights[0][count] = 1
+                yield lights, delay
+
+            for count in range(7): # phase 1, across top
+                lights[:][:] = 0
+                lights[count][6] = 1
+                yield lights, delay
+
+            for count in range(7): # phase 2, down side
+                lights[:][:] = 0
+                lights[6][6 - count] = 1
+                yield lights, delay
+
+            for count in range(7): # phase 3, back across bottom
+                lights[:][:] = 0
+                lights[6 - count][0] = 1
+                yield lights, delay
+
