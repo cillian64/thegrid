0567ac62a9dfab0d8444bc06e069ab6d08be0a28
David Turner <cillian64@googlemail.com>
2016-04-19 21:29:20 +0100

    Use all poles for channels, midi thru

--- control/thegrid/patterns/gridi.py
+++ control/thegrid/patterns/gridi.py
@@ -11,6 +11,7 @@ logger = logging.getLogger(__name__)
 
 import time
 import midi
+import midi.sequencer
 from colorsys import hsv_to_rgb
 
 @register_pattern("Gridi")
@@ -18,16 +19,35 @@ class Sample(Pattern):
     def __init__(self, cfg, tracking):
         logger.info("Starting Gridi")
         self.gen = self.generator()
-        self.midi_p = midi.read_midifile("thegrid/patterns/polka.mid")
+        self.midi_p = midi.read_midifile("thegrid/patterns/canyon.mid")
         self.midi_res = self.midi_p.resolution
-        self.midi_t = self.midi_p[np.argmax([len(t) for t in self.midi_p])]
+        self.events = self.midi_p[np.argmax([len(t) for t in self.midi_p])]
         self.t_delay = 60 / 120 / self.midi_res
         self.state = np.zeros((7, 7, 3), dtype=np.int)
 
+        self.midi_client = 128
+        self.midi_port = 0
+        self.hw = midi.sequencer.SequencerHardware()
+        self.seq = midi.sequencer.SequencerWrite(
+                sequencer_resolution=self.midi_res)
+        self.seq.subscribe_port(self.midi_client, self.midi_port)
+        self.seq.start_sequencer()
+
+        self.channels = []
+        for event in self.events:
+            if isinstance(event, midi.NoteOnEvent):
+                if event.channel not in self.channels:
+                    self.channels.append(event.channel)
+
+        self.poles_per_ch = 49 // len(self.channels)
+        self.channels.sort()
+
     def generator(self):
-        for event in self.midi_t:
+        for event in self.events:
             if event.tick != 0:
                 yield self.state, self.t_delay*event.tick
+            event.tick = 0
+            self.seq.event_write(event, False, False, True)
 
             if isinstance(event, midi.SetTempoEvent):
                 tempo = event.get_bpm()
@@ -38,10 +58,10 @@ class Sample(Pattern):
                 hue = (event.pitch % 12) / 12.0
                 rgbf = hsv_to_rgb(hue, 1.0, v)
                 rgb = tuple(int(x*255) for x in rgbf)
-                if event.channel <= 5:
-                    self.state[0, event.channel] = rgb
-                else:
-                    self.state[1, event.channel - 6] = rgb
+                firstpole = (self.channels.index(event.channel) *
+                             self.poles_per_ch)
+                self.state.reshape(49, 3)[firstpole:
+                        firstpole+self.poles_per_ch] = rgb
 
     def update(self):
         return next(self.gen)
