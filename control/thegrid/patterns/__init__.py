2538959cb2a5e7f05567d14dfd67115d7096798d
David Turner <cillian64@googlemail.com>
2016-07-22 22:31:02 +0100

    Only import patterns if deps exist

--- control/thegrid/patterns/__init__.py
+++ control/thegrid/patterns/__init__.py
@@ -7,7 +7,7 @@ Add any new patterns here with `from . import <name>`.
 
 from . import pong, amaze, sparkle, playlist, static_patterns, on, sample
 from . import pattern_one, strike, lightning
-from . import snake, diffusion, colourwheel, radar, vu
+from . import snake, diffusion, colourwheel, radar
 from .musicpatterns import thegrid
 try:
     import cv2
@@ -15,6 +15,18 @@ except:
     pass
 else:
     from . import simplecv
+try:
+    import alsaaudio
+except:
+    pass
+else:
+    from . import vu
+try:
+    import midi.sequencer
+except:
+    pass
+else:
+    from . import gridi
 
 from .pattern import loaded_patterns
 
