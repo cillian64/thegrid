e903a5be76d2ca51047b71391fc911f45c22745b
David Turner <cillian64@googlemail.com>
2016-07-31 22:35:14 +0100

    More work on kirk

--- control/thegrid/patterns/musicpatterns/captainkirk.py
+++ control/thegrid/patterns/musicpatterns/captainkirk.py
@@ -30,7 +30,7 @@ def roll_without_wrap(a, shift, axis):
                   {"filename": "kirk.wav",
                    "first_beat": 0.8,
                    "align_beat": 45.0,
-                   "align_beat_no": 95,
+                   "align_beat_no": 95.5,
                    "beats_per_bar": 4})
 class CaptainKirk(MusicPattern):
     def __init__(self, config, tracking):
@@ -55,7 +55,7 @@ class CaptainKirk(MusicPattern):
 
         # Lyrics start:
         # Red stepping with yellow pulse
-        if beat >= 33 and beat <= 60:
+        elif beat >= 33 and beat <= 60:
             print("Step and pulse: {}".format(barbeat))
             self.state[5, int(barbeat)] = (255, 0, 0)
             if barbeat in (2, 4) and beat_portion < 0.2:
@@ -63,14 +63,14 @@ class CaptainKirk(MusicPattern):
                 self.state[:, 6] = (255, 255, 0)
 
         # Breakdown before chorus: yellow pulse solo
-        if beat >= 61 and beat <= 64:
+        elif beat >= 61 and beat <= 64:
             print("Pulse: {}".format(barbeat))
             if barbeat in (2, 4) and beat_portion < 0.2:
                 self.state[:, 0] = (255, 255, 0)
                 self.state[:, 6] = (255, 255, 0)
 
         # First half of chorus - red/orange/yellow fuzz
-        if beat >= 65 and beat <= 96:
+        elif beat >= 65 and beat <= 96:
             for _ in range(10):
                 x, y = np.random.randint(0, 7, (2,))
                 self.state[x, y] = (np.random.randint(200, 256),
@@ -78,88 +78,143 @@ class CaptainKirk(MusicPattern):
                                     np.random.randint(0, 40))
 
         # Second half of chorus: variety of coloured fuzz per bar
-        if beat >= 97 and beat <= 104: # Pink/purple
+        elif beat >= 97 and beat <= 104: # Pink/purple
             for _ in range(10):
                 x, y = np.random.randint(0, 7, (2,))
                 self.state[x, y] = (np.random.randint(128, 256),
                                     0,
                                     np.random.randint(128, 256))
-        if beat >= 105 and beat <= 112: # Blue/cyan
+        elif beat >= 105 and beat <= 112: # Blue/cyan
             for _ in range(10):
                 x, y = np.random.randint(0, 7, (2,))
                 self.state[x, y] = (0,
                                     np.random.randint(128, 256),
                                     np.random.randint(128, 256))
-        if beat >= 113 and beat <= 120: # Shades of green:
+        elif beat >= 113 and beat <= 120: # Shades of green:
             for _ in range(10):
                 x, y = np.random.randint(0, 7, (2,))
                 self.state[x, y] = (0,
                                     np.random.randint(32, 256),
                                     0)
-        if beat >= 121 and beat <= 124: # Shades of grey:
+        elif beat >= 121 and beat <= 124: # Shades of grey:
             for _ in range(10):
                 x, y = np.random.randint(0, 7, (2,))
                 level = np.random.randint(64, 128)
                 self.state[x, y] = (level, level, level)
-        if beat >= 125 and beat <= 128: # White fuzz
+        elif beat >= 125 and beat <= 128: # White fuzz
             for _ in range(10):
                 x, y = np.random.randint(0, 7, (2,))
                 level = 255
                 self.state[x, y] = (level, level, level)
 
-        if beat >= 129:
-            print("Stop!")
+        # Back to the verse -- 4 steps around the outside of a square
+        elif beat >= 129 and beat <= 144:
+            self.state[1, barbeat] = (255, 0, 0)
+            self.state[5, 6 - barbeat] = (255, 0, 0)
 
-    
-#        if self.get_beat() > self.last_beat:
-#            print(self.get_beat())
-#            self.last_beat = self.get_beat()
+        # More verse.
+        # Now pulse the outside of the square on the offbeat and
+        # the inside of the square on the on-beat
+        elif beat >= 145 and beat <= 152:
+            self.state[1, barbeat + 1] = (255, 0, 0)
+            self.state[5, 6 - barbeat] = (255, 0, 0)
+            if barbeat in (1, 3) and beat_portion < 0.2:
+                self.state[2:5, 2:5] = (255, 255, 0)
+            if barbeat in (2, 4) and beat_portion < 0.2:
+                self.state[0:6, 0] = (255, 255, 0)
+                self.state[0:6, 6] = (255, 255, 0)
+                self.state[0, 0:6] = (255, 255, 0)
+                self.state[6, 0:6] = (255, 255, 0)
 
-        if beat < 95:
-            return self.state, 1.0
+        # Kirk is on the mountain. Checkerboard effect.
+        elif beat >= 153 and beat <= 156:
+            if barbeat == 1:
+                self.state[:, :] = (255, 0, 0)
+            elif barbeat == 2:
+                self.state[:3, :] = (255, 0, 0)
+            elif barbeat == 3:
+                self.state[:3, :3] = (255, 0, 0)
+                self.state[4:, 4:] = (255, 0, 0)
+            elif barbeat == 4:
+                for x in (0, 2, 4, 6):
+                    for y in (0, 2, 4, 6):
+                        self.state[x, y] = (255, 0, 0)
+        elif beat >= 157 and beat <= 160:
+            if barbeat in (1, 3):
+                for x in (1, 3, 5):
+                    for y in (1, 3, 5):
+                        self.state[x, y] = (255, 0, 0)
+            else:
+                for x in (0, 2, 4, 6):
+                    for y in (0, 2, 4, 6):
+                        self.state[x, y] = (255, 0, 0)
+
+        # Some more verse.  Spinner effect with varying colour.
+        elif beat >= 161 and beat <= 176:
+            if beat >= 161 and beat <= 168:
+                colour = (255, 0, 0)
+            else:
+                colour = (255, 128, 0)
+
+            if barbeat == 1:
+                self.state[1:6, 3] = colour
+            elif barbeat == 2:
+                for i in range(1, 6):
+                    self.state[i, 6-i] = colour
+            elif barbeat == 3:
+                self.state[3, 1:6] = colour
+            elif barbeat == 4:
+                for i in range(1, 6):
+                    self.state[i, i] = colour
+
+        # Some more verse.  Spinner effect with outer flash
+        elif beat >= 177 and beat <= 188:
+            if barbeat == 1:
+                self.state[1:6, 3] = (255, 255, 0)
+            elif barbeat == 2:
+                for i in range(1, 6):
+                    self.state[i, i] = (255, 255, 0)
+            elif barbeat == 3:
+                self.state[3, 1:6] = (255, 255, 0)
+            elif barbeat == 4:
+                for i in range(1, 6):
+                    self.state[i, 6-i] = (255, 255, 0)
+            if barbeat in (2, 4) and beat_portion < 0.2:
+                self.state[0:6, 0] = (255, 0, 0)
+                self.state[0:6, 6] = (255, 0, 0)
+                self.state[0, 0:6] = (255, 0, 0)
+                self.state[6, 0:6] = (255, 0, 0)
+
+        # Dark patch in the bar 189-193, with two flashes in the last beat:
+        elif beat == 192:
+            if ((beat_portion >= 0.0 and beat_portion <= 0.2)
+                or (beat_portion >= 0.5 and beat_portion <= 0.7)):
+                self.state[:, :] = (255, 255, 255)
+                print("BAP")
+
+        # God there's more verse.  Okay let's do some zoomout/pyramids.
+#        elif beat >= 193 and beat <= 200:
+#            colour = (255, 0, 0)
+#            if barbeat == 1:
+#                self.state[3, 3] = colour
+#            if barbeat == 2:
+#                self.state[2, 2:5] = colour
+#                self.state[4, 2:5] = colour
+#                self.state[3, 2] = colour
+#                self.state[3, 4] = colour
+#            if barbeat == 3:
+#                self.state[1, 1:6] = colour
+#                self.state[5, 1:6] = colour
+#                self.state[1:6, 1] = colour
+#                self.state[1:6, 5] = colour
+#            if barbeat == 4:
+#                self.state[0, 0:7] = colour
+#                self.state[6, 0:7
+
+
         else:
-            return self.state, 0.1
-#
-#        # Intro: sparkle
-#        if self.get_beat() == 0:
-#            state = np.zeros((7,7), dtype=np.bool).flatten()
-#            state[random.randrange(0, 48)] = True
-#            return state.reshape((7,7)), 0.05
-#
-#        # Talky bit: waves
-#        if self.get_beat() < 15:
-#            self.state = roll_without_wrap(self.state, 1, 1) # right
-#            if self.get_bar() > self.last_bar:
-#                self.last_bar += 1
-#                if self.get_bar() % 2 == 0:
-#                    self.state[:,0] = True
-#            return self.state, 0.1
-#
-#        # Waves from 3 directions
-#        if self.get_beat() == 15:
-#            self.state = roll_without_wrap(self.state, -1, 0) # up
-#            if self.get_beat() > self.last_beat:
-#                self.state[-1, :] = True
-#                self.last_beat = self.get_beat()
-#            return self.state, 0.05
-#        if self.get_beat() == 16:
-#            self.state = roll_without_wrap(self.state, -1, 1) # left
-#            if self.get_beat() > self.last_beat:
-#                self.state[:, -1] = True
-#                self.last_beat = self.get_beat()
-#            return self.state, 0.05
-#        if self.get_beat() == 17:
-#            self.state = roll_without_wrap(self.state, 1, 0) # down
-#            if self.get_beat() > self.last_beat:
-#                self.state[0, :] = True
-#                self.last_beat = self.get_beat()
-#            return self.state, 0.05
-#
-#        if self.get_beat() >= 65:
-#            self.state[:,:] = True
-#            return self.state, 0.1
-#
-#        # Fallback
-#        state = np.zeros((7,7), dtype=np.bool)
-#        return state, 0.05
-#        
+            self.state[:, :] = (0, 0, 0)
+#            print("Stop!")
+
+        return self.state, 0.1
+
