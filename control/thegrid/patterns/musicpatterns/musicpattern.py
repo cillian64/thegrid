60cc2ffdac07db6b66ed40bcdcbc69d101498c98
David Turner <cillian64@googlemail.com>
2016-07-31 21:26:58 +0100

    BPS tends to refer to BPS, not beat period...

--- control/thegrid/patterns/musicpatterns/musicpattern.py
+++ control/thegrid/patterns/musicpatterns/musicpattern.py
@@ -30,18 +30,18 @@ class MusicPattern(Pattern):
 
     def setup_bpm(self, config):
         self.first_beat = config["first_beat"]
-        self.bps = ((config["align_beat"] - config["first_beat"]) /
-                    config["align_beat_no"])
+        self.beat_period = ((config["align_beat"] - config["first_beat"]) /
+                            config["align_beat_no"])
         self.beats_per_bar = config["beats_per_bar"]
 
     def barbeat_to_t(self, bar, beat):
         return self.beat(bar*self.beats_per_bar + beat)
 
     def beat_to_t(self, beat):
-        return self.first_beat + self.bps * beat
+        return self.first_beat + self.beat_period * (beat - 1)
 
     def t_to_beat(self, t):
-        beat = (t - self.first_beat) // self.bps
+        beat = (t - self.first_beat) // self.beat_period
         return beat+1 if beat >= 0 else 0
 
     def t_to_barbeat(self, t):
@@ -50,6 +50,13 @@ class MusicPattern(Pattern):
         if beat == 0:
             beat = self.beats_per_bar
         return (bar, beat)
+
+    def t_to_beat_portion(self, t):
+        # Convert the time to a beat number, then find the time of the start
+        # of that beat.  This gives us the time into the current beat.  Divide
+        # this by the beat period (1/bps) to give the beat portion.
+        time_into_beat = t - self.beat_to_t(self.t_to_beat(t))
+        return time_into_beat / self.beat_period
 
     def get_time(self):
         return self.musicplayer.time
@@ -63,3 +70,5 @@ class MusicPattern(Pattern):
     def get_barbeat(self):
         return self.t_to_barbeat(self.get_time())
 
+    def get_beat_portion(self):
+        return self.t_to_beat_portion(self.get_time())
