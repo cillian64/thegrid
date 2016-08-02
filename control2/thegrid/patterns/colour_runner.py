407b969eebed5d70558a9ef8d292e9347e0c9231
Hannah McLaughlin <the.banannah@gmail.com>
2016-08-02 21:04:32 +0100

    Location now moves from (0, 0) to (0, 6), then stops.  Next step is to move through whole grid

--- control2/thegrid/patterns/colour_runner.py
+++ control2/thegrid/patterns/colour_runner.py
@@ -3,12 +3,12 @@ Colour runner pattern
 
 Looks as if a coloured pole is running through The Grid!  Imagine: grid is set
 to all blue.  Suddenly, one of them is red, and the co-ordinates of the red
-pole changes, leaving a fading red trail in its wake.  The colour of the runner
-(the red pole) changes, as does the background colour.  Exciting!
+pole changes, leaving a fading red trail in its wake.  We call this moving red
+pole the runner.  The colour of the runner changes, as does the background i
+colour.  Exciting!
 """
 
 import numpy as np
-import random
 import logging
 logger = logging.getLogger(__name__)
 
@@ -24,15 +24,22 @@ class ColourRunner(Pattern):
         self.grid_gen = self.generate_grid()
         self.runner_loc = self.runner_location()
 
+    def update(self):
+        return next(self.grid_gen), 1/10
+
     @staticmethod
     def runner_location():
         """Yields (x, y) co-ordinates of the runner"""
-        start = (random.randint(0, 7), random.randint(0, 7))
-        while True:
-            yield start
+        start = [0, 0]
+        yield start
 
-    def update(self):
-        return next(self.grid_gen), 1/10
+        loc = start
+        while True:
+            if loc[0] == 6:
+                loc = [loc[0] - 1, loc[1]]
+            else:
+                loc = [loc[0] + 1, loc[1]]
+            yield loc
 
     def generate_grid(self):
         """
