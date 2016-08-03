f8e1cb154b10ef3eab693d96bcfbc934f99d1a6c
David Turner <cillian64@googlemail.com>
2016-08-03 12:48:37 +0100

    More ayb

--- software/thegrid/patterns/musicpatterns/ayb.py
+++ software/thegrid/patterns/musicpatterns/ayb.py
@@ -20,7 +20,7 @@ logger = logging.getLogger(__name__)
                    "align_beat": 36.7,
                    "align_beat_no": 84,
                    "beats_per_bar": 4})
-@clicker()
+@clicker(vol=0)
 class CaptainKirk(MusicPattern):
     def __init__(self, config, ui):
         self.state = np.zeros((7, 7, 3), dtype=np.uint8)
@@ -34,6 +34,55 @@ class CaptainKirk(MusicPattern):
         beat_portion = self.get_beat_portion()
         logger.info("barbeat: {}, beat: {:03}".format(barbeat, beat))
         self.state[:] = 0
+
+        if beat == 0:
+            self.state[:, :] = (64, 0, 0)
+        elif beat <= 20:
+            if barbeat == 1:
+                self.state[:3, :3] = (255, 0, 0)
+            elif barbeat == 2:
+                self.state[:3, 4:] = (255, 0, 0)
+            elif barbeat == 3:
+                self.state[4:, 4:] = (255, 0, 0)
+            elif barbeat == 4:
+                self.state[4:, :3] = (255, 0, 0)
+        elif beat <= 36:
+            if barbeat in (2, 4):
+                self.state[:, :] = (255, 255, 0)
+        elif beat <= 52:
+            if barbeat in (2, 4):
+                self.state[:, :] = (255, 255, 0)
+            else:
+                self.state[:, :] = (255, 0, 0)
+        elif beat <= 56:
+            if barbeat in (2, 4):
+                self.state[:, 4:] = (255, 255, 0)
+            else:
+                self.state[:, :3] = (255, 0, 0)
+        elif beat <= 60:
+            if barbeat in (2, 4):
+                self.state[:3, :] = (255, 255, 0)
+            else:
+                self.state[4:, :] = (255, 0, 0)
+        elif beat <= 64:
+            if barbeat in (2, 4):
+                self.state[:, :3] = (255, 255, 0)
+            else:
+                self.state[:, 4:] = (255, 0, 0)
+        elif beat <= 68:
+            if barbeat in (2, 4):
+                self.state[4:, :] = (255, 255, 0)
+            else:
+                self.state[:3, :] = (255, 0, 0)
+        elif beat <= 76:
+            if beat_portion <= 0.2:
+                if barbeat in (1, 3):
+                    self.state[:, :] = (255, 80, 0)
+                else:
+                    self.state[:, :] = (255, 255, 0)
+        elif beat <= 84:
+            if beat_portion <= 0.2:
+                self.state[:, :] = (255, 255, 255)
 
 
 
