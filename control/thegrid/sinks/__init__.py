0f6d6a0ebdcb5481171b3d8c9b8c056ca1969ac2
Adam Greig <adam@adamgreig.com>
2016-08-03 03:10:51 +0100

    [cleanup 2] remove all old directories

--- control/thegrid/sinks/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-The sinks package contains all the sink modules, plus a decorator and base
-class in `sinks.sink`.
-
-Add any new sinks here with `from . import <name>`.
-"""
-
-from . import ftdi, tkinter, leonardo
-from .sink import loaded_sinks
