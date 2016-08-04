0f7e875758b5bae0f437f783786e5ab1b9eab90d
Adam Greig <adam@adamgreig.com>
2016-08-04 17:13:39 +0100

    updated beepety boop for linux

--- firmware/polefw/scripts/beepety_boop.sh
+++ firmware/polefw/scripts/beepety_boop.sh
@@ -6,17 +6,18 @@ while true
 do
 	if arm-none-eabi-gdb --batch \
                       -return-child-result \
-		              -ex 'target extended-remote /dev/cu.usbmodemD6CBAEC1' \
+		              -ex 'target extended-remote /dev/ttyACM0' \
 					  -ex 'monitor version' \
 					  -ex 'monitor swdp_scan' \
 					  -ex 'attach 1' \
 					  -ex 'load' \
-                      build/testfw.elf | grep Transfer
+                      ../build/polefw.elf | grep Transfer
     then
-        echo Programmed okay: sending magic
-        afplay tada.mp3
-        python3 quicktest.py
+        echo Programmed okay
+        mplayer -really-quiet tada.mp3 2>/dev/null
+        #python3 quicktest.py
         sleep 5
+        echo Trying again...
     else
         echo Fail!
         sleep 1
