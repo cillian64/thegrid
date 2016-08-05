75fe1b0711017bba5af22f40f411f59af70614c2
Adam Greig <adam@adamgreig.com>
2016-08-05 16:59:11 +0100

    move checksums to each packet

--- firmware/polefw/frame.h
+++ firmware/polefw/frame.h
@@ -11,15 +11,15 @@ typedef struct {
         struct {uint8_t sound_id, sound_freq, sound_mag, r, g, b; };
         struct {uint8_t cmd_id, payload[5]; };
     };
+    uint16_t checksum;
 } __attribute__((packed)) Packet;
 
 typedef struct {
     union {
-        uint8_t raw[302];
+        uint8_t raw[398];
         struct {
             uint8_t sync[6];
             Packet packets[49];
-            uint16_t checksum;
         };
     };
 } __attribute__((packed)) Frame;
